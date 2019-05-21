2ddc8f9274b334b50bd4106bde29e6e9ed6a52c6
Jacco Gritter <jacco.gritter@gmail.com>
2019-05-21 16:02:40 +0200

    racen is nu mogelijk

--- myscript.js
+++ myscript.js
@@ -1,24 +1,48 @@
 
-let subscribers = [];
+window.onload = function() { 
+    document.getElementById("myButton").addEventListener("click", startStopRace) 
+};
 
+let raceBezig = false;
+
+let subscribers = [];
+let rondeTimer;  // variable voor het zetten van de timer
 
 function scorebord(topic, data) {
-    console.log("Scorebord: " + data.rondetijd);
+    document.getElementsByClassName("tijd")[0].innerHTML = data.rondetijd;
 }
 
 function teamEngineer (topic, data) {
-    if (data.rondetijden) console.log("Team:" + data.rondetijden);
-    if (data.bandentemperatuur) console.log("Team:" + data.bandentemperatuur);
-    if (data.motorstatus) console.log("Team:" + data.motorstatus);
+    if (topic == "auto.rondetijden") document.getElementsByClassName("tijd")[1].innerHTML = data.rondetijd;
+    if (topic == "auto.bandentemperatuur") document.getElementById("bandentemp").innerHTML = data.bandentemperatuur + " deg. celsius";
+    if (topic == "auto.motorstatus") document.getElementById("motorstatus").innerHTML = data.motorstatus;
 }
 
 subscribers.push (PubSub.subscribe("auto", teamEngineer));              // subscribe to all subjects
 subscribers.push (PubSub.subscribe("auto.rondetijden", scorebord));     // subscribe to subject rondetijden only
 
+function startStopRace() {
+    if (!raceBezig) {
+        document.getElementById("myButton").innerHTML = "Stop de race!";
+        rondeTimer = setInterval(rondjesRijden, 2000);
+        raceBezig = true;
+    } else {
+        clearInterval(rondeTimer);
+        document.getElementById("myButton").innerHTML = "Herstart de race!";
+        raceBezig = false;
+    }
 
-PubSub.publish("auto.rondetijden", {rondetijd: 1.43});
-PubSub.publish("auto.bandentemperatuur", {bandentemperatuur: 95});
-PubSub.publish("auto.motorstatus", {motorstatus: "80%"});
+function rondjesRijden() {
+    let randomTijd = "2." + Math.floor(Math.random() * 60);
+    PubSub.publish("auto.rondetijden", {rondetijd: randomTijd});
 
-//console.log(subscribers[0]);
-//console.log(subscribers[1]);
+    let randomTemperatuur = Math.floor(Math.random() * 30) + 80;
+    PubSub.publish("auto.bandentemperatuur", {bandentemperatuur: randomTemperatuur});
+
+    let randomMotorstatus = "" + (Math.floor(Math.random() * 50) + 50) + "%";
+    PubSub.publish("auto.motorstatus", {motorstatus: randomMotorstatus});
+
+    } 
+
+
+}
