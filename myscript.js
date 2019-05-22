3115d6f7623af033f4dd56195621bcd1919c9548
Jacco Gritter <jacco.gritter@gmail.com>
2019-05-22 10:04:46 +0200

    changed all code to fully OOP

--- myscript.js
+++ myscript.js
@@ -3,13 +3,38 @@ window.onload = function() {
     document.getElementById("myButton").addEventListener("click", startStopRace) 
 };
 
+class RaceAuto {
+    constructor(teamnaam) {
+        this.teamnaam = teamnaam;
+        this.rondetijd = 0;
+        this.bandentemperatuur = 0;
+        this.motorstatus = 0;
+    }
 
+    carStats() {
 
-let raceBezig = false;
+        let randomTemperatuur = Math.floor(Math.random() * 30) + 80;
+        PubSub.publish("auto.bandentemperatuur", {bandentemperatuur: randomTemperatuur});
 
-let subscribers = [];
-let rondeTimer;  // variable voor het zetten van de timer
+        let randomMotorstatus = "" + (Math.floor(Math.random() * 50) + 50) + "%";
+        PubSub.publish("auto.motorstatus", {motorstatus: randomMotorstatus});
 
+        let randomTijd = "2." + Math.floor(Math.random() * 60);
+        PubSub.publish("auto.rondetijden", {rondetijd: randomTijd});
+        
+    }
+}
+
+class RaceEngineer {
+    constructor(naam) {
+        this.naam = naam;
+    }
+    getData(topic, data) {
+        if (topic == "auto.rondetijden") document.getElementsByClassName("tijd")[1].innerHTML = data.rondetijd;
+        if (topic == "auto.bandentemperatuur") document.getElementById("bandentemp").innerHTML = data.bandentemperatuur + " deg. celsius";
+        if (topic == "auto.motorstatus") document.getElementById("motorstatus").innerHTML = data.motorstatus;
+    }
+}
 
 
 const tijdwaarneming = {
@@ -19,18 +44,26 @@ const tijdwaarneming = {
     }
 }
 
-function scorebord(topic, data) {
-    document.getElementsByClassName("tijd")[0].innerHTML = data.rondetijd;
+const scorebord = {
+    updateBord : function(topic, data) {
+        document.getElementsByClassName("tijd")[0].innerHTML = data.rondetijd;
+    }
 }
 
-function teamEngineer (topic, data) {
-    if (topic == "auto.rondetijden") document.getElementsByClassName("tijd")[1].innerHTML = data.rondetijd;
-    if (topic == "auto.bandentemperatuur") document.getElementById("bandentemp").innerHTML = data.bandentemperatuur + " deg. celsius";
-    if (topic == "auto.motorstatus") document.getElementById("motorstatus").innerHTML = data.motorstatus;
-}
 
-subscribers.push (PubSub.subscribe("auto", teamEngineer));              // subscribe to all subjects
-subscribers.push (PubSub.subscribe("auto.rondetijden", scorebord));     // subscribe to subject rondetijden only
+let raceBezig = false;
+
+let subscribers = [];
+let raceDeelnemers = [];
+let raceEngineers = [];
+let rondeTimer;  // variable voor het zetten van de timer
+
+raceDeelnemers.push (new RaceAuto("Red Bull"));
+raceEngineers.push (new RaceEngineer("Harry"));
+
+
+subscribers.push (PubSub.subscribe("auto", raceEngineers[0].getData));   // subscribe to all subjects
+subscribers.push (PubSub.subscribe("auto.rondetijden", scorebord.updateBord));     // subscribe to subject rondetijden only
 
 function startStopRace() {
     if (!raceBezig) {
@@ -45,13 +78,8 @@ function startStopRace() {
 
 function rondjesRijden() {
 
-    tijdwaarneming.zendTijd();
-
-    let randomTemperatuur = Math.floor(Math.random() * 30) + 80;
-    PubSub.publish("auto.bandentemperatuur", {bandentemperatuur: randomTemperatuur});
-
-    let randomMotorstatus = "" + (Math.floor(Math.random() * 50) + 50) + "%";
-    PubSub.publish("auto.motorstatus", {motorstatus: randomMotorstatus});
+    
+    raceDeelnemers[0].carStats();
 
     } 
 
